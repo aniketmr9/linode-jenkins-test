577cf9f4b095b56bba18ee2dd6a6288a3a3d73fc
aniketmr9 <aniketmr9@gmail.com>
2021-10-09 21:04:49 +0530

    initial commit

---
+++ src/main/java/com/jenkins/test/controller/JenkinsTestController.java
@@ -0,0 +1,12 @@
+package com.jenkins.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsTestController {
+	@RequestMapping("/")
+	public String test() {
+		return "Hello Jenkins";
+	}
+}
