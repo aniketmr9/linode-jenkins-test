c88d30ef0d6226804ae85831678c04e6153a4cee
Aniket Maurya <aniketmr9@gmail.com>
2022-03-30 13:59:56 +0530

    message update

--- src/main/java/com/jenkins/test/controller/JenkinsTestController.java
+++ src/main/java/com/jenkins/test/controller/JenkinsTestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsTestController {
 	@RequestMapping("/")
 	public String test() {
-		return "Hello World";
+		return "Hello Aniket";
 	}
 }
