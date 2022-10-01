5729ae02172b8ecf8a20742d190c1742dfb6e872
Aniket Maurya <aniketmr9@gmail.com>
2022-10-01 20:25:27 +0530

    Cors fix

--- src/main/java/com/jenkins/test/controller/JenkinsTestController.java
+++ src/main/java/com/jenkins/test/controller/JenkinsTestController.java
@@ -3,6 +3,7 @@ package com.jenkins.test.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class JenkinsTestController {
 	@RequestMapping("/")
