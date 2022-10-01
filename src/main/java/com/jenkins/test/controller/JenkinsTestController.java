e2b770cb14b0087d9e5308741ef3e3caf5277918
aniketmr9 <aniketmr9@gmail.com>
2022-10-01 20:34:48 +0530

    response updated

--- src/main/java/com/jenkins/test/controller/JenkinsTestController.java
+++ src/main/java/com/jenkins/test/controller/JenkinsTestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsTestController {
 	@RequestMapping("/")
 	public String test() {
-		return "Hello Git!";
+		return "{ \"title\" : \"Hello Git!\"}";
 	}
 }
