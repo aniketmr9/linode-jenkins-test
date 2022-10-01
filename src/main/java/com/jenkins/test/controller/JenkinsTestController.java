0454519ce3733eabeba2bca70836ab3477e4dc55
aniketmr9 <aniketmr9@gmail.com>
2022-10-01 20:28:08 +0530

    cors import

--- src/main/java/com/jenkins/test/controller/JenkinsTestController.java
+++ src/main/java/com/jenkins/test/controller/JenkinsTestController.java
@@ -1,5 +1,6 @@
 package com.jenkins.test.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
