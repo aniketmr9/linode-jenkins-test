1c30d41889a5160935665ace6b820699e806626c
Aniket Maurya <aniketmr9@gmail.com>
2022-10-06 18:34:19 +0530

    Update JenkinsTestController.java
    Hindi test

--- src/main/java/com/jenkins/test/controller/JenkinsTestController.java
+++ src/main/java/com/jenkins/test/controller/JenkinsTestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsTestController {
 	@RequestMapping("/")
 	public String test() {
-		return "{ \"title\" : \"Welcome User!\"}";
+		return "{ \"title\" : \"समय 6:30 है!\"}";
 	}
 }
